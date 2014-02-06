a506cfa9a946975bfc9baabda8a79cedfeb86ddb
Sam Moorhead <Moorhead@Sams-MacBook-Air.local>
2014-02-06 18:46:12 -0500

    fewd final

---
+++ The Robe Life/The Robe Life/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#slider').leanSlider({'directionNav': '',});
+});
+var conainer = document.querySelector('#container');
+var msnry = new Masonry(container,{itemSelector:'.boxes', gutter:10});
+
+	
+	
